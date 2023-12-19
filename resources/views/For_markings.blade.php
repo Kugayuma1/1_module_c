51a2df4a933bea95bdfb8c11091927241ee91326
John Christopher Feria <yumakuga113@gmail.com>
2023-12-19 14:47:54 +0800

    for markings

---
+++ resources/views/For_markings.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Markings</title>
+</head>
+<body>
+    <h1>For markings</h1>
+</body>
+</html>
